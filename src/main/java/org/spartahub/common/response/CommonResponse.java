5ed7da1003b56c0d2cc645310065692e45e51b0c
yonggyo1981 <yonggyo00@kakao.com>
2026-03-30 20:45:59 +0900

    .

--- src/main/java/org/spartahub/common/response/CommonResponse.java
+++ src/main/java/org/spartahub/common/response/CommonResponse.java
@@ -1,5 +1,7 @@
 package org.spartahub.common.response;
 
+import org.slf4j.MDC;
+
 public record CommonResponse<T>(
         boolean success,
         String message,
@@ -7,10 +9,10 @@ public record CommonResponse<T>(
         String traceId // 로그 추적용 ID (MDC 연동)
 ) {
     public static <T> CommonResponse<T> success(T data) {
-        return new CommonResponse<>(true, "요청이 성공적으로 처리되었습니다.", data, org.slf4j.MDC.get("traceId"));
+        return new CommonResponse<>(true, "요청이 성공적으로 처리되었습니다.", data, MDC.get("traceId"));
     }
 
     public static <T> CommonResponse<T> success(String message, T data) {
-        return new CommonResponse<>(true, message, data, org.slf4j.MDC.get("traceId"));
+        return new CommonResponse<>(true, message, data, MDC.get("traceId"));
     }
 }
