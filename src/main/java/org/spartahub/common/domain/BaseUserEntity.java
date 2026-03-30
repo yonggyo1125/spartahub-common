41941e7de1cd0dfa052e5107d5ecb124cb492616
yonggyo1981 <yonggyo00@kakao.com>
2026-03-30 09:31:03 +0900

    .

--- src/main/java/org/spartahub/common/domain/BaseUserEntity.java
+++ src/main/java/org/spartahub/common/domain/BaseUserEntity.java
@@ -6,6 +6,7 @@ import org.spartahub.common.util.SecurityUtil;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.util.StringUtils;
 
 import java.time.LocalDateTime;
 
@@ -25,8 +26,8 @@ public abstract class BaseUserEntity extends BaseEntity {
     @Column(length=45, insertable = false)
     protected String deletedBy;
 
-    protected void delete() {
-        deletedBy = SecurityUtil.getCurrentUsername().orElse("SYSTEM");
-        deletedAt = LocalDateTime.now();
+    protected void delete(String deletedBy) {
+        this.deletedBy = StringUtils.hasText(deletedBy) ? deletedBy : SecurityUtil.getCurrentUsername().orElse("SYSTEM");
+        this.deletedAt = LocalDateTime.now();
     }
 }
